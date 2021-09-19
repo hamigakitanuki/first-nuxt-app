a7544965d121c09d4ff40aee3329a1d3a1a41835
hamigakitanuki <kakizaki0213@gmail.com>
2021-09-20 01:46:43 +0900

    購入まで

--- pages/cart.vue
+++ pages/cart.vue
@@ -24,7 +24,7 @@
       </li>
     </ul>
     <p>料金：{{ this.totalPrice() }}円</p>
-    <button class="btn btn-primary">購入する</button>
+    <button v-on:click="buy" class="btn btn-primary">購入する</button>
   </div>
 </template>
 
@@ -96,6 +96,41 @@ export default {
         .set({
           items:cart
         });
+    },
+    async buy(){
+      // 時間
+      let time = new Date().getTime()
+
+      /**
+       * ログイン中か確認
+       */
+      let user = this.$fb.auth().currentUser
+      if (!user){
+        alert('ログインしてください')
+        return
+      }
+      let email = user.email;
+
+      // storeに保存
+      await this.$fb
+        .firestore()
+        .collection("sells")
+        .doc(email+"_"+time.toString())
+        .set({
+          id:email+"_"+time.toString(),
+          email:email,
+          items:this.cart,
+          totalPrice:this.totalPrice()
+        });
+
+      // カートの中身を空にする
+      await this.$fb
+      .firestore()
+      .collection("cart")
+      .doc(email)
+      .set({items:[]})
+
+      this.$router.push("/");
     }
   },
   async mounted() {
